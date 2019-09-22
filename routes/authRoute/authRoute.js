f52b49b8ebbc002ec2a1491f4c3e996b1541895c
masonkarsevar <masonkarsevar@gmail.com>
2019-09-22 11:26:59 -0700

    register route works perfectly. WIP: login route

--- routes/authRoute/authRoute.js
+++ routes/authRoute/authRoute.js
@@ -9,4 +9,19 @@ router.get('/', (req, res) => {
     res.status(200).json({message: 'base auth route is printing something!!!'});
 });
 
+router.post('/register', (req, res) => {
+    let user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 14);
+    user.password = hash;
+
+    userDb.addUser(user) 
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(err => {
+            res.status(500).json({error: err})
+        })
+})
+
 module.exports = router;
