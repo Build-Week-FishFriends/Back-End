8ccab389ef3861be0333628eef5503f02cfd3af7
masonkarsevar <masonkarsevar@gmail.com>
2019-09-23 09:43:21 -0700

    will merge with jay

--- routes/logRoute/logModel.js
+++ routes/logRoute/logModel.js
@@ -10,18 +10,36 @@ function addLog(log) {
 } 
 
 function getAllWaterBodies() {
-    return db('water-bodies')
+    return db('water-bodies');
+}
+
+function getAllLogsByWaterBodyId(waterBodyId) {
+    return db('logs').where(waterBodyId)
+}
+
+function getLogsByUserId(userId) {
+    return db('logs').where(userId);
+}
+
+function getLogsByFishId(fishId) {
+    return db('logs').where(fishId);
 }
 
 function updateLog(id, log) {
-    return db('logs').where({id})
+    return db('logs').where({id}).update(log);
+}
+
+function deleteLog(id) {
+    return db('logs').where({id}).del();
 }
 
 function getLogByWaterBodyId(id) {
-    return db('logs').where({waterBodyId: id})
+    return db('logs').where({waterBodyId: id});
 }
 
 module.exports = {
     addLog,
     getAllWaterBodies,
+    updateLog,
+    deleteLog
 }
