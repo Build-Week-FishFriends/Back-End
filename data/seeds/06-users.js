4f7249d13bb888f66de724a217640fad14b3eb42
masonkarsevar <masonkarsevar@gmail.com>
2019-09-24 15:57:50 -0700

    application seeded

--- data/seeds/06-users.js
+++ data/seeds/06-users.js
@@ -6,12 +6,5 @@ exports.seed = function(knex) {
         {"username": "mason4", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
         {"username": "mason5", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
         {"username": "keith2", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "keith3", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "keith4", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "keith5", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "alice3", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "alice4", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "alice5", "password": "mason", "firstName": "mason", "lastName": "karsevar"},
-        {"username": "alice6", "password": "mason", "firstName": "mason", "lastName": "karsevar"}
       ]);
 };
