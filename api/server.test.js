fea34630ca93fd3f64f4d0f7baad32bec6096d89
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-25 14:09:29 -0700

    Hoping to merge

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('./server.js');
+const authRoute = require('../routes/authRoute/authRoute.js');
 const db = require('../data/dbConfig.js');
 
 describe('server.js tests', () => {
@@ -9,11 +10,21 @@ describe('server.js tests', () => {
 
     describe('/auth/register route', () => {
         it('should return id number of newly created user', () => {
-            return request(server)
+            const object = {username: 'alice', password: 'alice', firstName: 'Mason', lastName: 'Karsevar'}
+            return request(authRoute)
             .post('/auth/register') 
-            .send({username: 'mason', password: 'mason'})
+            .set("Accept", "application/json")
+            .expect("Content-Type", "application/json; charset=utf-8")
+            .send(object)
             .then(res => {
-                expect(res.body).toBe(1)
+                console.log(res)
+                expect(res.body).toBe({ id: 1,
+                    username: 'alice',
+                    password:
+                     '$2a$14$drP785YzyAb35ywtV0tFte1rGPFp5ftWKb.PyHxzvQqF9KNQ7uom6',
+                    firstName: 'Mason',
+                    lastName: 'Karsevar',
+                    email: null })
             })
         })
     })
@@ -22,16 +33,16 @@ describe('server.js tests', () => {
         it('should return a status 200 when given a pre-existing username and password', function() {
             return request(server)
                 .post('/auth/login')
-                .send({username: 'mason', password: 'mason'})
+                .send({username: 'alice', password: 'alice'})
                 .set("Accept", "application/json")
                 .expect("Content-Type", "application/json; charset=utf-8")
                 .expect(200)
-                .then(res => {
-                    request(server)
-                        .post('/auth/login')
-                        .send({username: 'mason', password: 'mason'})
-                        .expect(200)
-                })
+                // .then(res => {
+                //     request(server)
+                //         .post('/auth/login')
+                //         .send({username: 'mason', password: 'mason'})
+                //         .expect(200)
+                // })
         })
     })
 })
