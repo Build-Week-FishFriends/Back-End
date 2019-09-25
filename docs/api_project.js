7c7b4afffe073d74189f914d486d90ee2704bf47
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-09-25 13:27:40 -0700

    added API documentation within each router

---
+++ docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "Back-End",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-09-25T20:26:57.765Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
